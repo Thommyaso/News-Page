54f15783eb45fa7bd69d942392b23e0cd83b76bd
Thommyaso <120485877+Thommyaso@users.noreply.github.com>
2024-07-25 15:21:35 +0100

    Prevent multiple timeouts when toggling hamburger menu, create a shadow behind opened menu

--- main/js/main.js
+++ main/js/main.js
@@ -2,24 +2,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const hamburgerMenu = document.querySelector('#navBar');
     const body = document.querySelector('body');
+    const shadow = document.querySelector('.shadow');
     const root = document.documentElement;
     const computedStyle = getComputedStyle(root);
     const bodyPadding = parseInt(computedStyle.getPropertyValue('--body-padding').trim(), 10);
     const updatedPadding = window.innerWidth - document.documentElement.clientWidth + bodyPadding;
 
+    let timeoutId
+
     hamburger.addEventListener('click', () => {
         if(hamburgerMenu.classList.contains('navBar-active')) {
+            clearTimeout(timeoutId);
             hamburgerMenu.classList.toggle('navBar-active');
             body.style.paddingRight = '';
             body.classList.remove('no-scroll');
-            setTimeout(()=>{
+            shadow.classList.remove('shadow-active')
+            timeoutId = setTimeout(()=>{
                 hamburgerMenu.classList.toggle('navBar-hidden');
             },100)
         } else {
+            clearTimeout(timeoutId);
             hamburgerMenu.classList.toggle('navBar-hidden');
             body.style.paddingRight = `${updatedPadding}px`
             body.classList.add('no-scroll');
-            setTimeout(()=>{
+            shadow.classList.add('shadow-active');
+            timeoutId = setTimeout(()=>{
                 hamburgerMenu.classList.toggle('navBar-active');
                 scroll()
             },100)
